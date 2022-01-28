4611e3fdd1d51ac60d120f50d5c84a0d299dbf01
ahmedhasan2 <ahmed.hasan@s-force.org>
2022-01-28 21:53:16 +0530

    remove unused package

--- zippycustomer/src/main/java/com/zippycustomer/controllers/CustomerController.java
+++ zippycustomer/src/main/java/com/zippycustomer/controllers/CustomerController.java
@@ -7,7 +7,6 @@ import javax.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -110,13 +109,6 @@ public class CustomerController {
 		userAccountDto.setPassword(form.getPassword());
 		userAccountDto.setMobileNo(form.getMobileNo());
 		userAccountDto.setAddressDto(addressDto);
-		
-		
-		
-		
-		
-		
-		
 		
 		logger.info("calling the rest api registercuster");
 		long customerId = userAccountFeign.registerCustomer(userAccountDto);
